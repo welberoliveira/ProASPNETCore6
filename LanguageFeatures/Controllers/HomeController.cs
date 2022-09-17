3c559fa5a25945768fc51607982cf1bd57fe58b0
welberoliveira <welberoliveira3@gmail.com>
2022-09-17 20:33:40 -0300

    Interpolarização da saida de HomeController

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ public class HomeController : Controller
 {
     public ViewResult Index()
     {
-        Product[] products = Product.GetProducts();
-        return View(new string[] { products[0].Name });
+        Product?[] products = Product.GetProducts();
+        return View(new string[] {
+            $"Name: {products[0]?.Name}, Price: { products[0]?.Price }"
+        });
     }
 }
