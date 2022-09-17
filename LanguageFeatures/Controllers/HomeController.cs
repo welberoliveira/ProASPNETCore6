022166e8800f5bc83c94abeaffa2e8f5e1a02899
welberoliveira <welberoliveira3@gmail.com>
2022-09-17 13:49:52 -0300

    Inclusão da entidade Product

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace LanguageFeatures.Controllers;
+
+public class HomeController : Controller
+{
+    public ViewResult Index()
+    {
+        Product[] products = Product.GetProducts();
+        return View(new string[] { products[0].Name });
+    }
+}
